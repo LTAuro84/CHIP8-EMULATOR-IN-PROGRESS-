50a3a53170c963ddd7e85f659aeaee0d486eed79
Symbolic <sarrm20@icloud.com>
2025-11-22 21:00:07 -0400

    Working command lines

--- main.cpp
+++ main.cpp
@@ -1,15 +1,31 @@
+#include "cmdLineParser.h"
 #include "logger.h"
 
 using namespace std;
 
 int main (int argc, char** argv) {
+    TCmdLineParser cmdParser;
+    shared_ptr<TLogger> logger = TLogger::getInstance();
+    cmdParser.parseCmdLine(argc, argv);
+    
+    if(cmdParser.isLogLevelSet()) {
+        shared_ptr<TLogger> logger = TLogger::getInstance();
+        switch (cmdParser.getLogLevel()) {
+            case 0: logger->setLogLevel(ELogLevel::NONE); break;
+            case 1: logger->setLogLevel(ELogLevel::ERROR); break;
+            case 2: logger->setLogLevel(ELogLevel::WARN); break;
+            case 3: logger->setLogLevel(ELogLevel::INFO); break;
+            case 4: logger->setLogLevel(ELogLevel::DEBUG); break;
+        
+        }
+    }
 
-    std::shared_ptr<TLogger> logger = TLogger::getInstance();
+    if (!cmdParser.isRomeFileNameSet()) {
+        logger->log("No rom path provided", ELogLevel::ERROR);
+        exit(1);
+    }
 
-    logger->log("This is an ERROR message!!!", ELogLevel::ERROR);
-    logger->log("This is a WARNING message!!!", ELogLevel::WARN);
-    logger->log("This is an INFO message!!!", ELogLevel::INFO);
-    logger->log("This is a DEBUG message!!!", ELogLevel::DEBUG);
+    logger -> log("ROM Path: " + cmdParser.getRomFileName(), ELogLevel::INFO);
 
     return 0;
     
