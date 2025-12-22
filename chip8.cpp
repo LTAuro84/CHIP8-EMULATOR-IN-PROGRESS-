da0a6e72a52648e7a3bd008c743b5001918628cb
Symbolic <sarrm20@icloud.com>
2025-12-22 17:38:02 -0400

    chip8 main

--- chip8.cpp
+++ chip8.cpp
@@ -13,5 +13,33 @@ TChip8::~TChip8() {
 }
 
 void TChip8::init(string rom_path) {
+
+    for(auto i = 0; i < SCREEN_HEIGHT; i++)
+        for(auto j = 0; j < SCREEN_WIDTH; j++)
+            m_screen[i][j] = 0;
+
+    //clear ram
+    for (auto i = 0; i < TOTAL_RAM; i++)
+        m_ram[i] = 0;
+
+    // load fonts
+    for (auto i = 0; i < FONTSET_SIZE; i++)
+        m_ram[i] = FONTSET[i];
     
+    m_delay_timer = 0;
+    m_sound_timer = 0;
+
+    for (auto i = 0; i < NUM_KEYS; i++)
+        m_keys[i] = 0;
+    
+    m_key_pressed = false;
+        
+}
+
+void TChip8::run() {
+
+}
+
+void TChip8::deinit() {
+
 }
