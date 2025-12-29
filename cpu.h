b21f4c6f3d704b79961b7ba455815bbfa1d9d3df
Auro <sarrm20@icloud.com>
2025-12-29 21:27:37 +0000

    cpu header

---
+++ cpu.h
@@ -0,0 +1,36 @@
+#ifndef __CPU_H__
+#define __CPU_H__
+
+#include <cstdint>
+
+#include "chip8.h"
+#include "logger.h"
+
+#define NUM_V_REGISTERS 16
+#define PC_START 0X200
+
+class TCpu {
+
+    uint8_t data_registers[NUM_V_REGISTERS]; // 8 bit register
+    uint16_t index_register; // memory address pointer
+    uint16_t program_counter; 
+    uint16_t stack_pointer;
+    
+    uint16_t current_opcode;
+    uint16_t instruction;
+
+    TChip8* chip8_system;
+
+    std::shared_ptr<TLogger> m_logger;
+
+public:
+    TCpu(TChip8* system);
+    ~TCpu();
+    void init();
+    void fetch();
+    void decode();
+    void execute();
+    void deinit();
+};
+
+#endif
