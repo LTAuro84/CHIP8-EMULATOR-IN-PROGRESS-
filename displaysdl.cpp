14952973348db723e04c8b4e903a63b86f058303
Sebastian <sarrm20@icloud.com>
2026-02-22 13:54:06 -0400

    update

--- displaysdl.cpp
+++ displaysdl.cpp
@@ -12,9 +12,10 @@ TDisplaySDL::~TDisplaySDL() {
 }
 
 void TDisplaySDL::init() {
-    
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) { 
     string errorSdl(SDL_GetError());
     m_loggerInstance->log("SDL ERROR: " + errorSdl, ELogLevel::ERROR);
     exit(1);
+    }
 }
