ab97068c7719a1f7df657998664cef4aa0e6d725
Sebastian <sarrm20@icloud.com>
2026-02-22 13:50:28 -0400

    update

---
+++ displaysdl.cpp
@@ -0,0 +1,20 @@
+#include <cstdint>
+#include <SDL2/SDL.h>
+#include "displaysdl.h"
+using namespace std;
+
+TDisplaySDL::TDisplaySDL() {
+    m_loggerInstance = TLogger::getInstance();
+}
+
+TDisplaySDL::~TDisplaySDL() {
+
+}
+
+void TDisplaySDL::init() {
+    
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    string errorSdl(SDL_GetError());
+    m_loggerInstance->log("SDL ERROR: " + errorSdl, ELogLevel::ERROR);
+    exit(1);
+}
