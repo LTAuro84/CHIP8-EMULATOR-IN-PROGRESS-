4dbbec038fbf4d85cf98ba5109d7039ea1fb9543
Sebastian <sarrm20@icloud.com>
2026-03-02 15:48:20 -0400

    update

--- keyboardsdl.h
+++ keyboardsdl.h
@@ -13,5 +13,9 @@ class KeyboardSDL : public Keyboard {
     public:
         KeyboardSDL();
         ~KeyboardSDL();
+
+        virtual void init() override;
+        virtual void update(uint8_t* key_map, bool* running) override;
+        virtual void deinit() override;
 };
 #endif
