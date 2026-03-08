927b30accfc1c7cb3adc46de9973080cb803d96c
Sebastian <sarrm20@icloud.com>
2026-03-08 13:08:31 -0400

    update

--- soundsdl.h
+++ soundsdl.h
@@ -2,7 +2,26 @@
 #define __SOUNDSDL_H__
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_audio.h>
+#include <memory>
 #include "logger.h"
 #include "sound.h"
+
+using namespace std;
+
+class TSoundSDL : public TSound {
+    SDL_AudioSpec audioSpec;
+
+    shared_ptr<TLogger> m_logger;
+
+    public:
+        TSoundSDL();
+        ~TSoundSDL();
+
+        virtual void init() override;
+        virtual void playTune() override;
+        virtual void pauseTune() override;
+        virtual void deinit() override;
+};
 
 #endif
